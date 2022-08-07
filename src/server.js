8ff17d6393c54ce5fd4b788d4496dd777403fe13
mamonde456 <mamonde456@gmail.com>
2022-08-07 23:21:28 +0900

    .

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 import apiRouter from "./routers/apiRouter";
-import { localsMiddleware } from "./middlewares";
+import { localsMiddleware } from "./middleware";
 
 const app = express();
 const logger = morgan("dev");
