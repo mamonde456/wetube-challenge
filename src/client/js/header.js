b236cf0ee85ace378e9b16a6f31a938566b6e41a
mamonde456 <mamonde456@gmail.com>
2022-11-17 16:12:57 +0900

    css fix

--- src/client/js/header.js
+++ src/client/js/header.js
@@ -1,42 +1,57 @@
 const header = document.querySelector("header");
-const menu = document.getElementById("menuIcon");
+const headerWatch = document.querySelector("#headerWrap");
+const menu = document.querySelector("#menuIconWrap");
 const li = document.querySelectorAll("#headerList li");
 const a = document.querySelectorAll("#headerList li a");
 const icon = document.querySelectorAll("#headerList li a i");
 const span = document.querySelectorAll("#headerList li a span");
 
+const { set } = headerWatch.dataset;
+console.log(set);
+
 const handleMenuOpen = () => {
   menu.removeEventListener("click", handleMenuOpen);
   menu.addEventListener("click", handleMenuClose);
-  header.style.width = "250px";
-  menu.style.textAlign = "";
-  for (const item of a) {
-    item.style.display = "flex";
-    item.style.alignItems = "center";
+
+  if (set) {
+    headerWatch.style.left = 0;
   }
-  for (const item of icon) {
-    item.style.fontSize = "28px";
-  }
-  for (const item of span) {
-    item.style.fontSize = "18px";
+  if (!set) {
+    header.style.width = "250px";
+    menu.style.textAlign = "";
+    for (const item of a) {
+      item.style.display = "flex";
+      item.style.alignItems = "center";
+    }
+    for (const item of icon) {
+      item.style.fontSize = "28px";
+    }
+    for (const item of span) {
+      item.style.fontSize = "18px";
+    }
   }
 };
 
-const handleMenuClose = () => {
+const handleMenuClose = (e) => {
   menu.removeEventListener("click", handleMenuClose);
   menu.addEventListener("click", handleMenuOpen);
-  header.style.width = "80px";
-  menu.style.textAlign = "center";
+  if (set) {
+    headerWatch.style.left = "-250px";
+  }
+  if (!set) {
+    header.style.width = "80px";
+    menu.style.textAlign = "center";
 
-  for (const item of a) {
-    item.style.display = "";
-    item.style.alignItems = "";
-  }
-  for (const item of icon) {
-    item.style.fontSize = "25px";
-  }
-  for (const item of span) {
-    item.style.fontSize = "13px";
+    for (const item of a) {
+      item.style.display = "";
+      item.style.alignItems = "";
+    }
+    for (const item of icon) {
+      item.style.fontSize = "25px";
+    }
+    for (const item of span) {
+      item.style.fontSize = "13px";
+    }
   }
 };
 
